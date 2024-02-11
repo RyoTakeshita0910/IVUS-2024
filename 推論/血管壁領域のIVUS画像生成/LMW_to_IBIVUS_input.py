c266e1d655c33917826df2830aa7d75f34edf22f
RyoTakeshita0910 <104045526+RyoTakeshita0910@users.noreply.github.com>
2024-02-12 04:42:22 +0900

    prediction

--- 推論/血管壁領域のIVUS画像生成/LMW_to_IBIVUS_input.py
+++ 推論/血管壁領域のIVUS画像生成/LMW_to_IBIVUS_input.py
@@ -23,6 +23,7 @@ args = parser.parse_args()
 w = 512
 h = 512
 
+# 出力先のフォルダにLumen, Media, Wireの保存先を作成
 def make_dir(path):
     if not os.path.isdir(os.path.join(path,"Lumen")):
         os.mkdir(os.path.join(path,"Lumen"))
@@ -33,12 +34,13 @@ def make_dir(path):
     if not os.path.isdir(os.path.join(path,"Wire")):
         os.mkdir(os.path.join(path,"Wire"))
 
+# LumenとMediaに関するラベル削除メソッド
 def pick_label(retval, labels, stats):
-    height = labels.shape[0]
-    width = labels.shape[1]
-
+    # 画像の中心におけるラベル番号を取得
     label_num = labels[255][255]
 
+    # 画像の中心におけるラベル番号が背景である場合 
+    # -> 面積が背景の次に大きいラベルのラベル番号を取得
     if label_num == 0:
         label_list = []
         for i in range(retval):
@@ -53,51 +55,48 @@ def pick_label(retval, labels, stats):
         sec_area = max(label_list)
         label_num = label_list.index(sec_area)
 
-    for h in range(height):
-        for w in range(width):
-            if labels[h][w] == label_num:
-                labels[h][w] = 255
+    # 取得したラベル番号以外のラベルを削除する
+    for y in range(h):
+        for x in range(w):
+            if labels[y][x] == label_num:
+                labels[y][x] = 255
             else:
-                labels[h][w] = 0
+                labels[y][x] = 0
 
     return labels.astype('uint8')
 
-
+# Wireのラベル削除に関するメソッド
 def pick_label_wire(retval, labels, stats):
-    height = labels.shape[0]
-    width = labels.shape[1]
-
     label_list = []
-
+    # 面積が2000[pixel]以上150000[pixel]以下のラベルのラベル番号を全て取得
     for i in range(retval):
         area = stats[i][-1]
         if area >= 2000 and area < 150000:
             label_list.append(i)
 
-    for h in range(height):
-        for w in range(width):
-            if labels[h][w] in label_list:
-                labels[h][w] = 255
+    # 取得したラベル番号以外のラベルを削除
+    for y in range(h):
+        for x in range(w):
+            if labels[y][x] in label_list:
+                labels[y][x] = 255
             else:
-                labels[h][w] = 0
+                labels[y][x] = 0
 
     return labels.astype('uint8')
 
-
+# マスク画像の調整：画素値が0未満 -> 画素値を0にする
 def set_trace(img):
     for y in range(h):
         for x in range(w):
             if img[y][x] < 0:
                 img[y][x] = 0
 
-
+# IVUS画像にマスク画像を適用する
 def set_input(img,bi):
-    h = img.shape[0]
-    w = img.shape[1]
-
     for y in range(h):
         for x in range(w):
             bi_c = bi[y][x]
+            # 背景の色をシアンにする
             if bi_c == 0:
                 img[y][x] = [255,255,0]
 
@@ -105,7 +104,7 @@ def set_input(img,bi):
 
 
 def main():
-
+    # Lumen, Media, Wireの予測領域のリストアップ
     lumen_list = sorted(os.listdir(args.lumen_dir))
     media_list = sorted(os.listdir(args.media_dir))
     wire_list = sorted(os.listdir(args.wire_dir))
@@ -125,11 +124,11 @@ def main():
     for l in lumen_list:
         l_img = cv2.imread(os.path.join(args.lumen_dir,l),cv2.IMREAD_GRAYSCALE)
 
-        # Otsu
+        # 大津の二値化
         _, l_otsu = cv2.threshold(l_img, 0, 255, cv2.THRESH_OTSU)
         cv2.imwrite(os.path.join(otsu_path,"Lumen",l), l_otsu)
 
-        # cut
+        # ラベル削除
         l_ret, l_labels, l_stats, _ = cv2.connectedComponentsWithStats(l_otsu)
         l_cut = pick_label(l_ret, l_labels, l_stats)
         l_contours, _ = cv2.findContours(l_cut, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -141,11 +140,11 @@ def main():
     for m in media_list:
         m_img = cv2.imread(os.path.join(args.media_dir,m),cv2.IMREAD_GRAYSCALE)
 
-        # Otsu
+        # 大津の二値化
         _, m_otsu = cv2.threshold(m_img, 0, 255, cv2.THRESH_OTSU)
         cv2.imwrite(os.path.join(otsu_path,"Media",m), m_otsu)
 
-        # cut
+        # ラベル削除
         m_ret, m_labels, m_stats, _ = cv2.connectedComponentsWithStats(m_otsu)
         m_cut = pick_label(m_ret, m_labels, m_stats)
         m_contours, _ = cv2.findContours(m_cut, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -158,11 +157,11 @@ def main():
     for w in wire_list:
         w_img = cv2.imread(os.path.join(args.wire_dir,w),cv2.IMREAD_GRAYSCALE)
 
-        # Otsu
+        # 大津の二値化
         _, w_otsu = cv2.threshold(w_img, 0, 255, cv2.THRESH_OTSU)
         cv2.imwrite(os.path.join(otsu_path,"Wire",w), w_otsu)
 
-        # cut
+        # ラベル削除
         w_ret, w_labels, w_stats, _ = cv2.connectedComponentsWithStats(w_otsu)
         w_cut = pick_label(w_ret, w_labels, w_stats)
         w_contours, _ = cv2.findContours(w_cut, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -190,6 +189,7 @@ def main():
                         m_img = cv2.imread(os.path.join(cut_path,'Media',m), cv2.IMREAD_GRAYSCALE)
                         w_img = cv2.imread(os.path.join(cut_path,'Wire',w), cv2.IMREAD_GRAYSCALE)
 
+                        # 血管壁のマスク画像を作成
                         trace_gray = m_img.astype(float) - l_img.astype(float) - w_img.astype(float)
                         set_trace(trace_gray)
                     
@@ -197,6 +197,7 @@ def main():
                         for img in img_list:
                             if img == l:
                                 ivus_img = cv2.imread(os.path.join(args.img_dir, img))
+                                # IVUS画像にマスク画像を適用
                                 seg_input = set_input(ivus_img, trace_gray)
                                 cv2.imwrite(os.path.join(out_path, img), seg_input)
                                 print(img)  
